866038fd45ee52735283653496c48778a313fcc2
Dmitry <df.creative@gmail.com>
2017-02-08 21:49:30 +0300

    Render borders

--- lib/shaders/frag.glsl
+++ lib/shaders/frag.glsl
@@ -10,17 +10,25 @@ varying vec2 charOffset;
 varying vec2 pointCoord;
 varying float pointSize;
 
+const float borderWidth = 1.;
+
 void main() {
 	vec2 pointUV = (pointCoord - gl_FragCoord.xy + pointSize * .5) / pointSize;
 	pointUV.x = 1. - pointUV.x;
 	vec2 texCoord = ((charOffset + pointUV) * charsStep) / charsShape;
 	float dist = texture2D(chars, texCoord).r;
 
-	float buffer = .725;
-	float gamma = .0275 * 50. / pointSize;
+	// border color
+	float dif = borderWidth / pointSize;
+	float borderLevel = .75 - dif;
+	float charLevel = .75 + dif;
+	float gamma = .005 * charsStep / pointSize;
 
-	float alpha = smoothstep(buffer - gamma, buffer + gamma, dist);
-	gl_FragColor = vec4(charColor.rgb, alpha * charColor.a);
+	float alpha = smoothstep(borderLevel - gamma, borderLevel + gamma, dist);
+	float mixAmount = smoothstep(charLevel - gamma, charLevel + gamma, dist);
+
+	//mix border color and color
+	gl_FragColor = vec4(mix(charColor.rgb, borderColor.rgb, 1. - mixAmount), alpha * borderColor.a);
 
 	// gl_FragColor = vec4(vec3(1.-dist), 1.);
 }
